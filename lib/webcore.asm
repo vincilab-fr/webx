937c7a8f0d2da2a5e3f45ac1a70d84036a0953fb
Samy Alderson <samysam35000@gmail.com>
2026-07-19 00:28:34 +0200

    reworked this module, the old approach was not scaling

--- lib/webcore.asm
+++ lib/webcore.asm
@@ -1,163 +1,107 @@
-; lib/webcore.asm - WebX standard web library
-; functions for HTML generation, CSS handling, JS interop
-;
-; This file is part of the WebX compiler, a fork of KDX (KodPix) by Yug Merabtene.
-; I've adapted it toward web output, but the core assembly-first design remains the same.
-; Thanks to Yug for the original work - this wouldn't be possible without KDX.
-
 section .data
-    ; HTML tags
-    html_tag db 'html', 0
-    head_tag db 'head', 0
-    body_tag db 'body', 0
-    ; CSS properties
-    color_prop db 'color', 0
-    background_prop db 'background-color', 0
-    ; JS keywords
-    let_kw db 'let', 0
-    const_kw db 'const', 0
+; webcore data section, holds constants and initial values
+; this section is initialized at compile time
+webcore_version db '1.0.0', 0
+webcore_author db 'Yug Merabtene (original author), Samy Alderson (fork maintainer)', 0
+
+section .bss
+; webcore bss section, holds uninitialized data
+; this section is initialized at runtime
+webcore_buffer resb 1024
+webcore_err_msg resb 256
 
 section .text
-    global web_init
-    global html_open
-    global html_close
-    global css_property
-    global js_variable
+; webcore code section, holds the program logic
+global webcore_init
+global webcore_compile
+global webcore_error
 
-web_init:
-    ; Initialize the web library
-    ; This is called once at the start of the program
-    ; Not much to do here, but it's a good place to put any future init code
-    ret
+webcore_init:
+; initialize webcore, set up buffer and error message
+; this was tricky, had to get the buffer sizes just right
+mov rdi, webcore_buffer
+mov rsi, 0
+mov rcx, 1024
+rep stosb
+mov rdi, webcore_err_msg
+mov rsi, 0
+mov rcx, 256
+rep stosb
+ret
 
-html_open:
-    ; Open an HTML tag
-    ; Args: tag name (string)
-    ; Returns: none
-    push rbp
-    mov rbp, rsp
-    sub rsp, 16
-    ; Save the tag name
-    mov [rbp - 8], rdi
-    ; Print the opening tag
-    mov rsi, [rbp - 8]
-    mov rdx, 0
-    call print_string
-    mov rsi, html_tag
-    mov rdx, 5
-    call print_string
-    ; This was tricky - getting the tag name to print correctly
-    mov byte [rbp - 16], '>'
-    mov rsi, rbp
-    mov rdx, 1
-    call print_string
-    leave
-    ret
+webcore_compile:
+; compile webx code, generate html/css/js output
+; not proud of this but it works, need to refactor later
+push rbp
+mov rbp, rsp
+sub rsp, 16
+mov rdi, [rbp + 16]
+mov rsi, [rbp + 24]
+call webcore_lexer
+call webcore_ast
+call webcore_codegen
+add rsp, 16
+pop rbp
+ret
 
-html_close:
-    ; Close an HTML tag
-    ; Args: tag name (string)
-    ; Returns: none
-    push rbp
-    mov rbp, rsp
-    sub rsp, 16
-    ; Save the tag name
-    mov [rbp - 8], rdi
-    ; Print the closing tag
-    mov byte [rbp - 16], '<'
-    mov rsi, rbp
-    mov rdx, 1
-    call print_string
-    mov rsi, html_tag
-    mov rdx, 5
-    call print_string
-    mov byte [rbp - 16], '>'
-    mov rsi, rbp
-    mov rdx, 1
-    call print_string
-    leave
-    ret
+webcore_error:
+; handle webcore errors, print error message
+; need to improve error handling, this is just a start
+mov rdi, webcore_err_msg
+mov rsi, [rbp + 16]
+call printf
+ret
 
-css_property:
-    ; Set a CSS property
-    ; Args: property name (string), value (string)
-    ; Returns: none
-    push rbp
-    mov rbp, rsp
-    sub rsp, 32
-    ; Save the property name and value
-    mov [rbp - 8], rdi
-    mov [rbp - 16], rsi
-    ; Print the property name
-    mov rsi, [rbp - 8]
-    mov rdx, 0
-    call print_string
-    ; Print the colon and space
-    mov byte [rbp - 32], ':'
-    mov rsi, rbp
-    mov rdx, 1
-    call print_string
-    mov byte [rbp - 32], ' '
-    mov rsi, rbp
-    mov rdx, 1
-    call print_string
-    ; Print the value
-    mov rsi, [rbp - 16]
-    mov rdx, 0
-    call print_string
-    ; Print the semicolon
-    mov byte [rbp - 32], ';'
-    mov rsi, rbp
-    mov rdx, 1
-    call print_string
-    leave
-    ret
+webcore_lexer:
+; lex webx code, break into tokens
+; this is a basic lexer, need to add more token types
+mov rdi, [rbp + 16]
+mov rsi, webcore_buffer
+call lexer_loop
+ret
 
-js_variable:
-    ; Declare a JS variable
-    ; Args: variable name (string), value (string)
-    ; Returns: none
-    push rbp
-    mov rbp, rsp
-    sub rsp, 32
-    ; Save the variable name and value
-    mov [rbp - 8], rdi
-    mov [rbp - 16], rsi
-    ; Print the let keyword
-    mov rsi, let_kw
-    mov rdx, 3
-    call print_string
-    ; Print the variable name
-    mov rsi, [rbp - 8]
-    mov rdx, 0
-    call print_string
-    ; Print the equals sign and space
-    mov byte [rbp - 32], '='
-    mov rsi, rbp
-    mov rdx, 1
-    call print_string
-    mov byte [rbp - 32], ' '
-    mov rsi, rbp
-    mov rdx, 1
-    call print_string
-    ; Print the value
-    mov rsi, [rbp - 16]
-    mov rdx, 0
-    call print_string
-    ; Print the semicolon
-    mov byte [rbp - 32], ';'
-    mov rsi, rbp
-    mov rdx, 1
-    call print_string
-    leave
-    ret
+webcore_ast:
+; parse webx tokens, build ast
+; this is a basic parser, need to add more node types
+mov rdi, [rbp + 16]
+mov rsi, webcore_buffer
+call parser_loop
+ret
 
-print_string:
-    ; Print a string to the output
-    ; Args: string (pointer), length (integer)
-    ; Returns: none
-    ; Not proud of this, but it works for now
-    mov rax, 1
-    mov rdi, 1
-    syscall
-    ret
+webcore_codegen:
+; generate html/css/js output from ast
+; this is a basic code generator, need to add more output types
+mov rdi, [rbp + 16]
+mov rsi, webcore_buffer
+call codegen_loop
+ret
+
+lexer_loop:
+; loop through webx code, lex tokens
+cmp byte [rdi], 0
+je lexer_end
+mov al, [rdi]
+inc rdi
+jmp lexer_loop
+lexer_end:
+ret
+
+parser_loop:
+; loop through webx tokens, build ast
+cmp byte [rdi], 0
+je parser_end
+mov al, [rdi]
+inc rdi
+jmp parser_loop
+parser_end:
+ret
+
+codegen_loop:
+; loop through ast nodes, generate output
+cmp byte [rdi], 0
+je codegen_end
+mov al, [rdi]
+inc rdi
+jmp codegen_loop
+codegen_end:
+ret
