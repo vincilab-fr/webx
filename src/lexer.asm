feb16301fb1a6f65b3b2ebbc16a68c6342e80f66
Samy Alderson <samysam35000@gmail.com>
2026-07-22 00:52:40 +0200

    this was driving me nuts, reorganized the whole thing

--- src/lexer.asm
+++ src/lexer.asm
@@ -1,138 +1,66 @@
 ; src/lexer.asm
+; Improved version of lexer code
+; Copyright (c) 2026 Samy Alderson
 
 section .text
-global _start
+global lexer
 
-_start:
-    ; Lexer entry point
+lexer:
+    ; Read a character from the input file
+    push rsi
+    mov rsi, rdi
+    mov rdi, 1
+    mov rax, 0
+    syscall
+    pop rsi
 
-; Lexer function to scan for a token
-scan_token:
-    ; Clear the buffer to avoid leftover characters
-    mov rsi, token_buffer
-    mov rdi, 64 ; Buffer size
-    sub rsi, rdi ; Point to the start of the buffer
-    xor rax, rax ; Clear the register
-    rep stosb ; Zero the buffer
-    mov rsi, token_buffer ; Move the buffer pointer
+    ; Check if we've reached the end of the file
+    cmp al, 0
+    je end_lexer
 
-    ; Check for a keyword
-    call check_keyword
-    jz FOUND_KEYWORD
-    mov rsi, token_buffer ; Reset the buffer pointer
+    ; Convert the character to a token type
+    cmp al, 'a'
+    jl skip_char
+    cmp al, 'z'
+    jg skip_char
+    cmp al, '_'
+    jne skip_char
+    movzx eax, al
+    sub eax, 0x61
+    add eax, 0x09
+    jmp process_token
 
-    ; Check for an identifier
-    call check_identifier
-    jz FOUND_IDENTIFIER
-    mov rsi, token_buffer ; Reset the buffer pointer
+skip_char:
+    cmp al, '0'
+    jl skip_char
+    cmp al, '9'
+    jg skip_char
+    cmp al, '-'
+    jne skip_char
+    movzx eax, al
+    sub eax, 0x30
+    jmp process_token
 
-    ; Check for a number
-    call check_number
-    jz FOUND_NUMBER
-    mov rsi, token_buffer ; Reset the buffer pointer
+skip_char:
+    cmp al, '+'
+    jne skip_char
+    mov eax, 0x40
 
-    ; Check for a string
-    call check_string
-    jz FOUND_STRING
-    mov rsi, token_buffer ; Reset the buffer pointer
+process_token:
+    ; Convert token type to ASCII character
+    add eax, 0x30
+    mov [result], al
 
-    ; If none of the above, assume it's a symbol
-    mov rsi, token_buffer
-    mov rdi, 1 ; Symbol type
-    mov [rsi + 4], rdi ; Store the symbol type
-    xor rax, rax ; Clear the register
-    rep stosb ; Zero the rest of the buffer
-    jmp FOUND_SYMBOL
-
-FOUND_KEYWORD:
-    mov rsi, token_buffer
-    mov rdi, 2 ; Keyword type
-    mov [rsi + 4], rdi ; Store the keyword type
-    xor rax, rax ; Clear the register
-    rep stosb ; Zero the rest of the buffer
-    jmp FOUND_TOKEN
-
-FOUND_IDENTIFIER:
-    mov rsi, token_buffer
-    mov rdi, 3 ; Identifier type
-    mov [rsi + 4], rdi ; Store the identifier type
-    xor rax, rax ; Clear the register
-    rep stosb ; Zero the rest of the buffer
-    jmp FOUND_TOKEN
-
-FOUND_NUMBER:
-    mov rsi, token_buffer
-    mov rdi, 4 ; Number type
-    mov [rsi + 4], rdi ; Store the number type
-    xor rax, rax ; Clear the register
-    rep stosb ; Zero the rest of the buffer
-    jmp FOUND_TOKEN
-
-FOUND_STRING:
-    mov rsi, token_buffer
-    mov rdi, 5 ; String type
-    mov [rsi + 4], rdi ; Store the string type
-    xor rax, rax ; Clear the register
-    rep stosb ; Zero the rest of the buffer
-    jmp FOUND_TOKEN
-
-FOUND_SYMBOL:
-    mov rsi, token_buffer
-    mov rdi, 1 ; Symbol type
-    mov [rsi + 4], rdi ; Store the symbol type
-    xor rax, rax ; Clear the register
-    rep stosb ; Zero the rest of the buffer
-
-FOUND_TOKEN:
-    ; Return the token
+    ; Return the result
+    mov rax, result
     ret
 
-; Token buffer
-token_buffer times 64 db 0
-
-section .data
-    ; Keyword tokens
-    ; ... (add keywords here)
-
-    ; Identifier tokens
-    ; ... (add identifiers here)
-
-    ; Number tokens
-    ; ... (add numbers here)
-
-    ; String tokens
-    ; ... (add strings here)
-
-section .bss
-    ; Buffer for the current token
-    token_buffer times 64 resb 1
-
-; Functions for checking tokens
-%macro CHECK_TOKEN 1
-%assign macro_number %1
-    section .text
-    global check_macro_%%macro_number
-check_macro_%%macro_number:
-    ; Check if the token matches the macro
-    ; ... (add implementation here)
-    jmp check_token_default
-%endmacro
-
-; ... (add CHECK_TOKEN macros here)
-
-section .text
-check_token_default:
-    ; Default token check (e.g., just return an error)
-    ; ... (add implementation here)
+end_lexer:
+    ; Return a special token to indicate the end of the file
+    mov eax, 0xFF
+    mov [result], al
+    mov rax, result
     ret
 
 section .data
-    ; Token types
-    TYP_KEYWORD equ 2
-    TYP_IDENTIFIER equ 3
-    TYP_NUMBER equ 4
-    TYP_STRING equ 5
-    TYP_SYMBOL equ 1
-    TYP_UNKNOWN equ 0
-
-; ... (add token types here)
+result times 1 db 0
