e6f94d340954d0c5b45714531a7f8bf2fcc41044
Samy Alderson <samysam35000@gmail.com>
2026-07-24 16:29:26 +0200

    fixed some ugly code I wrote last week

--- src/linker.asm
+++ src/linker.asm
@@ -1,80 +1,66 @@
 section .text
 global _start
-
 _start:
-    ; Load the start address of the .data section
-    mov rsi, .data
+    ; Improve improve_code
+    ; We need to resolve external references and create a symbol table
+    ; This is a simplified example, real-world implementations would be more complex
+    ; We assume the symbol table is stored in a data structure, for now it's just a simple array
+    ; We also assume the external references are stored in a separate data structure
 
-; Load the address of the .data section
-.data:
-    ; Reserve space for the symbol table
-    times 1024 db 0
+    ; Load the symbol table and external references
+    mov rsi, symbol_table ; Load the symbol table into RSI
+    mov rdx, external_references ; Load the external references into RDX
 
-section .bss
-    ; Reserve space for the symbol table
-    resb 1024
+    ; Initialize the linker status
+    mov r8, 0 ; Set the linker status to 0 (no errors)
+    mov r9, 0 ; Set the output offset to 0
+
+linker_loop:
+    ; Get the next symbol and reference from the tables
+    mov r10, [rsi] ; Get the next symbol
+    mov r11, [rdx] ; Get the next reference
+
+    ; Check if there are more symbols and references
+    cmp r10, 0 ; If the symbol is 0, there are no more symbols
+    jz done ; Exit the loop
+    cmp r11, 0 ; If the reference is 0, there are no more references
+    jz done ; Exit the loop
+
+    ; Resolve the external reference
+    ; We assume the reference is a label, we need to find its address
+    ; We also assume the symbol table contains the address of the symbol
+    mov r12, [r10 + r9] ; Get the address of the symbol
+    mov r13, r12 ; Copy the address into R13
+    mov [r11 + r9], r13 ; Store the address in the reference
+
+    ; Move to the next symbol and reference
+    add rsi, 8 ; Move to the next symbol
+    add rdx, 8 ; Move to the next reference
+    add r9, 8 ; Move to the next output offset
+
+    jmp linker_loop
+
+done:
+    ; The linking process is complete, we can now output the linked code
+    ; We assume the output is stored in a data structure, for now it's just a simple array
+    ; We also assume the data structure contains the size of the output
+    mov rsi, output ; Load the output into RSI
+    mov rdx, [output_size] ; Load the output size into RDX
+
+    ; Write the output to the file
+    mov rax, 1 ; System call for write
+    mov rdi, 1 ; File descriptor for stdout
+    mov rsi, rsi ; Pointer to the output
+    mov rdx, rdx ; Size of the output
+    syscall
+
+    ; Exit the program
+    mov rax, 60 ; System call for exit
+    xor rdi, rdi ; Return code 0
+    syscall
 
 section .data
-    ; Symbol table offset
-    symtab_offset equ 0
-    ; Symbol table size
-    symtab_size equ 1024
-
-; Load the start address of the .bss section
-section .bss
-    ; Reserve space for the symbol table
-    resb 1024
-
-; Load the end address of the .bss section
-section .bss
-    ; Reserve space for the symbol table
-    resb 1024
-
-; Load the end address of the .data section
-section .data
-    ; Reserve space for the symbol table
-    resb 1024
-
-; Link the symbol table
-link_symbol_table:
-    ; Load the start address of the .data section
-    mov rsi, .data
-
-    ; Load the address of the symbol table
-    mov rdi, symtab_offset
-
-    ; Load the size of the symbol table
-    mov rdx, symtab_size
-
-    ; Call the linker
-    call linker
-
-; Define the linker function
-linker:
-    ; Initialize the symbol table
-    mov [rsi + rdi], rsi
-
-    ; Iterate over the symbol table
-    mov rcx, 1024
-loop:
-    ; Check if the symbol table is full
-    cmp rcx, 0
-    je end_loop
-
-    ; Check if the symbol table contains a null symbol
-    mov rax, [rsi + rdi]
-    test rax, rax
-    jz end_loop
-
-    ; Move to the next symbol in the table
-    add rdi, 8
-
-    ; Decrement the loop counter
-    dec rcx
-
-    ; Jump to the next iteration
-    jmp loop
-
-end_loop:
-    ; Return from the linker
-    ret
+symbol_table times 16 dq 0 ; Symbol table
+external_references times 16 dq 0 ; External references
+output times 4096 db 0 ; Output buffer
+output_size times 4 db 0 ; Output size
